69d3953bc1c9585fab18cb74c20fe3f499002f99
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-09-10 16:04:51 +0200

    Enable start/stop ADX with multiple clusters

--- StartAdxCluster/__init__.py
+++ StartAdxCluster/__init__.py
@@ -9,19 +9,23 @@ from azure.mgmt.kusto import KustoManagementClient
 def main(startadxtimer: func.TimerRequest) -> None:
 
     SUBSCRIPTION_ID = os.environ["AZURE_SUBSCRIPTION_ID"]
-    ADX_RESOURCE_GROUP = os.environ["ADX_RESOURCE_GROUP"]
-    ADX_CLUSTER_NAME = os.environ["ADX_CLUSTER_NAME"]
+    ADX_CLUSTERS_CONFIG = json.loads(os.environ["ADX_CLUSTERS_CONFIG"])
+    # ADX_CLUSTERS_CONFIG=[{"cluster_name": "cluster1", "resource_group": "group1"}, {"cluster_name": "cluster2", "resource_group": "group2"}, ...]
 
     client = KustoManagementClient(credential=DefaultAzureCredential(), subscription_id=SUBSCRIPTION_ID)
 
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
     
-    poller = client.clusters.begin_start(
-        resource_group_name=ADX_RESOURCE_GROUP,
-        cluster_name=ADX_CLUSTER_NAME,
-    )
-    if poller.done():
-        logging.info(f"Started Kusto Cluster {ADX_CLUSTER_NAME}: {ADX_RESOURCE_GROUP}...")
+    for cluster_config in ADX_CLUSTERS_CONFIG:
+        resource_group = cluster_config['resource_group']
+        cluster_name = cluster_config['cluster_name']
+        
+        poller = client.clusters.begin_start(
+            resource_group_name=resource_group,
+            cluster_name=cluster_name,
+        )
+        if poller.done():
+            logging.info(f"Started Kusto Cluster {cluster_name} in {resource_group}...")
 
-    logging.info(f"Python timer trigger function ran {utc_timestamp}")
+    logging.info(f"Python timer trigger function ran at {utc_timestamp}")
